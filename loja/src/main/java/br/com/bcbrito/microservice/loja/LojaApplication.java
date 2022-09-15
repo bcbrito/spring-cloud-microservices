15c7f35a51c263c49fa64f044aa4d2dee770986e
bcbrito <developer.bcbrito@gmail.com>
2022-09-15 13:33:53 -0300

    Implementando Eureka server e correção de configs
    Implementando Eureka server e correção de configs

--- loja/src/main/java/br/com/bcbrito/microservice/loja/LojaApplication.java
+++ loja/src/main/java/br/com/bcbrito/microservice/loja/LojaApplication.java
@@ -2,10 +2,19 @@ package br.com.bcbrito.microservice.loja;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class LojaApplication {
 
+	@Bean
+	@LoadBalanced
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(LojaApplication.class, args);
 	}
--- loja/src/main/java/br/com/bcbrito/microservice/loja/service/CompraService.java
+++ loja/src/main/java/br/com/bcbrito/microservice/loja/service/CompraService.java
@@ -1,5 +1,6 @@
 package br.com.bcbrito.microservice.loja.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -10,10 +11,12 @@ import br.com.bcbrito.microservice.loja.controller.dto.InfoFornecedorDTO;
 
 @Service
 public class CompraService {
+	
+	@Autowired
+	private RestTemplate client;
 
 	public void realizaCompra(CompraDTO compra) {
-
-		RestTemplate client = new RestTemplate();
+		
 		ResponseEntity<InfoFornecedorDTO> exchange =  client.exchange("http://fornecedor/info/"+compra.getEndereco().getEstado(), HttpMethod.GET, null, InfoFornecedorDTO.class);
 
 		System.out.println(exchange.getBody().getEndereço());
