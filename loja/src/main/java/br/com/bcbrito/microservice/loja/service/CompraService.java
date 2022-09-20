4b7f6c487cb90a5af48086ac3ea803dd340c3e28
Bruno Caetano de Brito <developer.bcbrito@gmail.com>
2022-09-20 15:38:52 -0300

    Inspecionando portas load balance

--- loja/src/main/java/br/com/bcbrito/microservice/loja/service/CompraService.java
+++ loja/src/main/java/br/com/bcbrito/microservice/loja/service/CompraService.java
@@ -1,6 +1,7 @@
 package br.com.bcbrito.microservice.loja.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -15,9 +16,16 @@ public class CompraService {
 	@Autowired
 	private RestTemplate client;
 
+	@Autowired
+	private DiscoveryClient eurekaClient;
+
 	public void realizaCompra(CompraDTO compra) {
 		
 		ResponseEntity<InfoFornecedorDTO> exchange =  client.exchange("http://fornecedor/info/"+compra.getEndereco().getEstado(), HttpMethod.GET, null, InfoFornecedorDTO.class);
+
+		eurekaClient.getInstances("fornecedor").stream().forEach(fornecedor -> {
+			System.out.println("localhost:" + fornecedor.getPort());
+		});
 
 		System.out.println(exchange.getBody().getEndereço());
 	}
